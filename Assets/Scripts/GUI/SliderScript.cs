f29e038ffc7147082b32b12b46e32a004428e2f3
Robert <v22robert@gmail.com>
2019-04-18 12:14:07 +0200

    Slider GUI

---
+++ Assets/Scripts/GUI/SliderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public float minimumHouseAmount;
+    public Slider DestructionMeter;
+    
+    void Start()
+    {
+        DestructionMeter.maxValue = GameObject.FindGameObjectsWithTag("house").Length;
+        DestructionMeter.minValue = minimumHouseAmount;  
+    }
+
+    
+    void Update()
+    {
+        DestructionMeter.value = GameObject.FindGameObjectsWithTag("house").Length;        
+    }
+}
