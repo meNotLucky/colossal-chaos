94463d579e6d6a3f405af64422018da307945527
Lukas Clausson <lukas.clausson@hotmail.com>
2019-04-29 12:55:32 +0200

    Added Stopping Ability

---
+++ Assets/Scripts/Utility/EngineExtentions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+ 
+static public class EngineExtensions
+{
+    
+    static public T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+    {
+        return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
+    }
+}
