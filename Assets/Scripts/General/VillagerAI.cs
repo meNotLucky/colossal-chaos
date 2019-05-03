529c9bbbf9623f5708f596b05c71e3c8492dfc52
Robert <v22robert@gmail.com>
2019-05-03 10:30:48 +0200

    Merge

---
+++ Assets/Scripts/General/VillagerAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VillagerAI : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public float timeForNewPath;
+    private bool inCoroutine;
+    Vector3 target;
+    NavMeshPath path;
+    bool validPath;
+
+        void Start()
+    {
+        navMeshAgent=GetComponent<NavMeshAgent>();
+        path=new NavMeshPath();
+    }
+
+    Vector3 getNewPos()
+    {
+        float x = Random.Range(285,500);
+        float z= Random.Range(450,710);
+        Vector3 pos=new Vector3(x,transform.position.y,z);
+        return pos;
+    }
+
+    IEnumerator WaitForNextPath()
+    {
+        inCoroutine=true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        validPath=navMeshAgent.CalculatePath(target,path);
+        while(!validPath)
+        {
+            yield return new WaitForSeconds(0.01f);
+            GetNewPath();
+            validPath=navMeshAgent.CalculatePath(target,path);
+
+        }
+        inCoroutine=false;
+    }
+    void GetNewPath()
+    {
+        target=getNewPos();
+        navMeshAgent.SetDestination(target);
+    }
+    void Update()
+    {
+        if(!inCoroutine)
+        {
+            StartCoroutine(WaitForNextPath());
+        }
+    }
+}
