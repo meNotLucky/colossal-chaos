6ef05297e601223895d02b6412f4ba9265af5324
Lukas Clausson <lukas.clausson@hotmail.com>
2019-05-07 16:59:21 +0200

    Added Side Stepping + animations

---
+++ Assets/Scripts/GUI/DepressionEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Rendering.PostProcessing;
+
+public class DepressionEffect : MonoBehaviour
+{
+    [Header("PP-Reference")]
+    public GameObject postProcessing;
+    private PostProcessProfile profile;
+    private ColorGrading color;
+    private Grain grain;
+    private Vignette vignette;
+
+    void Start()
+    {
+        profile = postProcessing.GetComponent<PostProcessVolume>().profile;
+
+        color = profile.GetSetting<ColorGrading>();
+        grain = profile.GetSetting<Grain>();
+        vignette = profile.GetSetting<Vignette>();
+    }
+
+    void Update()
+    {
+        color.saturation.value = 0 - GetComponent<LooseCondition>().destroyedHouses;
+
+        if(color.saturation.value > 0)
+            color.saturation.value = 0;
+    }
+}
---
+++ Assets/Scripts/General/LooseCondition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCondition : MonoBehaviour
+{
+    public GameObject slider;
+    [HideInInspector] public int numberOfHouses;
+    [HideInInspector] public int destroyedHouses = 0;
+
+    void Start()
+    {
+        numberOfHouses = GameObject.FindGameObjectsWithTag("house").Length;
+    }
+
+    void Update()
+    {
+        slider.GetComponent<SliderScript>().SetMinMaxValue(0, GlobalSettings.maxDestroyedHouses);
+        destroyedHouses = numberOfHouses - GameObject.FindGameObjectsWithTag("house").Length;
+
+        slider.GetComponent<SliderScript>().SetValue(destroyedHouses);
+
+        if(destroyedHouses >= GlobalSettings.maxDestroyedHouses)
+        {
+            if(FindObjectOfType<DeathScreen>() != null)
+                FindObjectOfType<DeathScreen>().EndGame();
+        }  
+        
+    }
+    
+}
