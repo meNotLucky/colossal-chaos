8ba630896737eb6c1076cf597f1aef015620ded8
Lukas Clausson <lukas.clausson@hotmail.com>
2019-05-08 16:08:41 +0200

    Finished the First Pop Ups

---
+++ Assets/Scripts/MenuUI/PopUps/PulsePopUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PulsePopUp : MonoBehaviour
+{
+    public Color startColor;
+    public Color endColor;
+    public float speed;
+
+    void Update()
+    {
+        float t = Mathf.Sin(Time.realtimeSinceStartup * speed);
+        GetComponent<Image>().color = Color.Lerp(startColor, endColor,t);
+        GetComponentInChildren<TextMeshProUGUI>().color = Color.Lerp(startColor, endColor, t);
+
+    }
+}
