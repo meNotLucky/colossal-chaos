91ecb35037294fb18f71d1463eeb64b139cf684f
Lukas Clausson <lukas.clausson@hotmail.com>
2019-05-17 11:45:44 +0200

    Removed null files

--- Assets/Scripts/MenuUI/MainMenuScript.cs
+++ Assets/Scripts/MenuUI/MainMenuScript.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using UnityEngine.Rendering.PostProcessing;
+
 public class MainMenuScript : MonoBehaviour
 {
-
 	public UnityEngine.UI.Button myButton;
 	public Slider PlaySlider;
+	public Animator animator;
+	public PostProcessProfile profile;
 	public float timeToChange = 1f;
-	public float mouseHeldTimer = 0f;
-	public float timer = 0.1f;
-	public float colortimer;
+	private float mouseHeldTimer = 0f;
+	private float timer = 0.1f;
+	private float colortimer;
+	private float originalBloomValue;
 
 void Start()
 {
 	Cursor.lockState = CursorLockMode.Locked;
 	Time.timeScale = 1f;
+
+	if(profile != null){
+		originalBloomValue = profile.GetSetting<Bloom>().intensity.value;
+	}
 }
 	void Update()
 	{
 
-	if(Input.GetAxisRaw("Mouse X")!=0)
+	if(Input.GetAxisRaw("Mouse X") != 0)
 	{
 		Cursor.lockState = CursorLockMode.None;
 	}
@@ -34,18 +42,35 @@ void Start()
 			mouseHeldTimer += Time.deltaTime;
 			//myButton.Select();
 
+			if(animator != null){
+				animator.SetBool("Hover", true);
+			}
+
+			if(profile != null){
+				profile.GetSetting<Bloom>().intensity.value *= 1.012f;
+			}
 
 			if(PlaySlider.value < 1f){
 				PlaySlider.value += timer / 60;
 			}
 
-			if(PlaySlider.value >= 1){
+			if(mouseHeldTimer >= timeToChange){
+				if(profile != null){
+					profile.GetSetting<Bloom>().intensity.value = originalBloomValue;
+				}
 				GetComponent<LoadGameScript>().LoadGame();
 				Destroy(this);
 			}
 		}
 		else
 		{
+			if(animator != null){
+				animator.SetBool("Hover", false);
+			}
+
+			if(profile != null){
+				profile.GetSetting<Bloom>().intensity.value = originalBloomValue;
+			}
 
 			if(PlaySlider.value >= 0)
 				PlaySlider.value -= timer / 60;
