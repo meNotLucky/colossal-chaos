7240197c2b263d7691e7c7a59328ca2c20aff750
Lukas Clausson <lukas.clausson@hotmail.com>
2019-05-22 10:53:34 +0200

    Began work on camera animation

---
+++ Assets/LevelStartScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStartScript : MonoBehaviour
+{
+    public Camera paningCamera;
+    public Animator cameraAnimator;
+
+    void Start()
+    {
+    }
+}
