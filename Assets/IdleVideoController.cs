0c72450133517d6e16d8f57dca2d302f90b17185
Lukas Clausson <lukas.clausson@hotmail.com>
2019-06-05 01:39:02 +0200

    Added Idle Main Menu Video

---
+++ Assets/IdleVideoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Video;
+
+public class IdleVideoController : MonoBehaviour
+{
+    public GameObject idleVideo;
+    public GameObject GUI;
+    public float timeBeforeIdle;
+
+    private float timer;
+
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0){
+            timer = 0;
+            idleVideo.SetActive(false);
+            GUI.SetActive(true);
+        }
+
+        if(timer < timeBeforeIdle)
+            timer += Time.deltaTime;
+        else {
+            idleVideo.SetActive(true);
+            GUI.SetActive(false);
+        }
+    }
+}
